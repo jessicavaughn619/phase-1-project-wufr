8c9ef64bd5cf0b227b2c89485e094ff18ad9469f
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-01-22 13:48:15 -0700

    Render one dog, add dynamic index number

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 // Sets toggle display to none
 let addDog = false;
+let allDogs = [];
 
 document.addEventListener("DOMContentLoaded", function() {
     fetchDogs();
+    setTimeout(() => {renderDog(allDogs, 0)}, 5000);
 });
 
 // Initial get request from dog API
-const dogUrl = "https://dog.ceo/api/breeds/image/random/50";
+const dogUrl = "https://dog.ceo/api/breeds/image/random/10";
 
 function fetchDogs() {
     fetch(dogUrl)
     .then(res => res.json())
-    .then(json => renderDog(json))
+    .then(json => makeDogArray(json.message))
 };
 
-function renderDog(dogs) {
-    let allDogs = dogs.message;
+function makeDogArray(json) {
+    for (item of json) {
+        allDogs.push(item);
+    };
+};
+
+function renderDog(dogs, num) {
     const main = document.getElementById("dog-card-container");
     const img = document.createElement("img");
     img.className = "dog-image";
-    img.src = allDogs[0];
+    img.src = dogs[num];
     img.style.width = "auto"
     img.style.height = "400px";
     main.appendChild(img);
@@ -38,19 +45,19 @@ const dogFavoritesContainer = document.getElementById("dog-favorites-container")
   });
 
 // Post request
-function saveDog(event){
-    fetch("", {
-      method: "POST",
-      headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    },
-    body: JSON.stringify({
-      "message": 
-    })
-    })
-    .then(res => res.json())
-    };
+// function saveDog(event){
+//     fetch("", {
+//       method: "POST",
+//       headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json"
+//     },
+//     body: JSON.stringify({
+//       "message": 
+//     })
+//     })
+//     .then(res => res.json())
+//     };
 
 // Listen for left or right arrow
 document.addEventListener("keydown", function(event) {
@@ -62,12 +69,10 @@ function dogEvent(key) {
             case "ArrowLeft":
                 console.log("Pressed left arrow!")
                 break;
-            
             case "ArrowRight":
                 console.log("Pressed right arrow!")
                 saveDog();
                 break;
-
             default: console.log();
         }  
 };

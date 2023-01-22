a5ad9f43474deed4f4f06f72602cb4e3a5f16989
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-01-22 12:46:28 -0700

    Update fetch request to include 50 dogs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # phase-1-project-wufr
+
+Paw-print image: https://toppng.com/free-image/lion-paw-print-clipart-clip-art-PNG-free-PNG-Images_217030
+
--- index.js
+++ index.js
@@ -1,35 +1,67 @@
+// Sets toggle display to none
+let addDog = false;
+
 document.addEventListener("DOMContentLoaded", function() {
     fetchDogs();
 });
 
-const dogUrl = "https://dog.ceo/api/breeds/image/random";
+// Initial get request from dog API
+const dogUrl = "https://dog.ceo/api/breeds/image/random/50";
 
 function fetchDogs() {
     fetch(dogUrl)
     .then(res => res.json())
-    .then(json => renderDogs(json))
+    .then(json => renderDog(json))
 };
 
-function renderDogs(dogs) {
+function renderDog(dogs) {
     let allDogs = dogs.message;
-    console.log(allDogs)
     const main = document.getElementById("dog-card-container");
     const img = document.createElement("img");
     img.className = "dog-image";
-    img.src = allDogs;
+    img.src = allDogs[0];
     img.style.width = "400px"
     img.style.heigh = "auto";
     main.appendChild(img);
 };
 
-// function renderDog(dogs) {
-//     const dogArray = dogs.message;
-//     const dogCards = document.getElementById("dog-card-container");
-//     dogArray.forEach(dog => {
-//     const card = document.createElement("div");
-//     card.src = dog;
-//     card.className = "card";
-//     dogCards.appendChild(card);
-//     });
-// };
+// Toggle favorites container
+const favoritesBtn = document.getElementById("favorite-dog-button");
+const dogFavoritesContainer = document.getElementById("dog-favorites-container");
+  favoritesBtn.addEventListener("click", () => {
+    addDog = !addDog;
+    if (addDog) {
+      dogFavoritesContainer.style.display = "block";
+    } else {
+      dogFavoritesContainer.style.display = "none";
+    }
+  });
 
+// Post request
+function saveDog() {
+
+};
+
+// Listen for left or right arrow
+$(dogCardContainer).on("swipeleft", function() {
+    console.log("Next dog please")
+});
+
+$(dogCardContainer).on("swiperight", function() {
+    console.log("Save this dog")
+});
+
+// function dogEvent(key) {
+//     switch (key) {
+//             case "ArrowLeft":
+//                 console.log("Pressed left arrow!")
+//                 break;
+            
+//             case "ArrowRight":
+//                 console.log("Pressed right arrow!")
+//                 saveDog();
+//                 break;
+
+//             default: console.log();
+//         }  
+// }
